d90cc4839b6f4cf2c3ebcf17668c0c90332c9453
Matthew Jones <matthew.jones@ebth.com>
2021-10-26 14:06:05 -0400

    Basic push notification code

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import * as Notifications from "expo-notifications";
 import React, { useState, useEffect, useRef } from "react";
 import { Text, View, Platform } from "react-native";
 
+// Controls the behavior or incoming push notifications. 
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
     shouldShowAlert: true,
@@ -13,27 +14,37 @@ Notifications.setNotificationHandler({
 
 export default function App() {
   const [pushToken, setPushToken] = useState("");
+  // We have the ability to react to incoming notifications while the user is in-app. 
+  // The "notification" state lets us know if a push notification has been received. 
   const [notification, setNotification] = useState(false);
+
   const notificationListener = useRef();
   const responseListener = useRef();
 
+  // Handles requesting a push token from the OS. 
   registerForPushNotificationsAsync = async () => {
+    // Push doesn't work on simulators. 
     if (Constants.isDevice) {
+
+      // Check to see if we have permissions to send notifications
       const { status: existingStatus } =
         await Notifications.getPermissionsAsync();
 
       let finalStatus = existingStatus;
 
+      // Request permission if we don't have it. 
       if (existingStatus !== "granted") {
         const { status } = await Notifications.requestPermissionsAsync();
         finalStatus = status;
       }
 
+      // If the user says no, do all the sad things. 
       if (finalStatus !== "granted") {
         alert("Failed to get push token for push notification!");
         return;
       }
 
+      // Grab the push token and store it in state
       const token = (await Notifications.getDevicePushTokenAsync()).data;
 
       console.log(token);
@@ -43,6 +54,7 @@ export default function App() {
       alert("Must use physical device for Push Notifications");
     }
 
+    // Android gives us bonus configuration options, so we can config them here. 
     if (Platform.OS === "android") {
       Notifications.setNotificationChannelAsync("default", {
         name: "default",
@@ -53,6 +65,7 @@ export default function App() {
     }
   };
 
+  // Upon rendering, register for push. 
   useEffect(() => {
     registerForPushNotificationsAsync();
 
@@ -68,6 +81,7 @@ export default function App() {
         console.log(response);
       });
 
+    // Remove the subscriptions and listeners on app close (For testing)
     return () => {
       Notifications.removeNotificationSubscription(
         notificationListener.current
