87bf862ee800ff08a3974b765564576d813ae5c3
Matthew Jones <matthew.jones@ebth.com>
2021-11-01 11:04:38 -0400

    Added application package

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import Constants from "expo-constants";
 import * as Notifications from "expo-notifications";
+import * as Application from 'expo-application';
 import React, { useState, useEffect, useRef } from "react";
 import { Text, View, Platform } from "react-native";
 
-// Controls the behavior or incoming push notifications. 
+// Controls the behavior or incoming push notifications.
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
     shouldShowAlert: true,
@@ -14,47 +15,53 @@ Notifications.setNotificationHandler({
 
 export default function App() {
   const [pushToken, setPushToken] = useState("");
-  // We have the ability to react to incoming notifications while the user is in-app. 
-  // The "notification" state lets us know if a push notification has been received. 
+  const [idfv, setIdfv] = useState("");
+
+  // We have the ability to react to incoming notifications while the user is in-app.
+  // The "notification" state lets us know if a push notification has been received.
   const [notification, setNotification] = useState(false);
 
   const notificationListener = useRef();
   const responseListener = useRef();
 
-  // Handles requesting a push token from the OS. 
+  // Handles requesting a push token from the OS.
   registerForPushNotificationsAsync = async () => {
-    // Push doesn't work on simulators. 
+    // Push doesn't work on simulators.
     if (Constants.isDevice) {
-
       // Check to see if we have permissions to send notifications
       const { status: existingStatus } =
         await Notifications.getPermissionsAsync();
 
       let finalStatus = existingStatus;
 
-      // Request permission if we don't have it. 
+      // Request permission if we don't have it.
       if (existingStatus !== "granted") {
         const { status } = await Notifications.requestPermissionsAsync();
         finalStatus = status;
       }
 
-      // If the user says no, do all the sad things. 
+      // If the user says no, do all the sad things.
       if (finalStatus !== "granted") {
         alert("Failed to get push token for push notification!");
         return;
       }
 
-      // Grab the push token and store it in state
+      // Grab the push token
       const token = (await Notifications.getDevicePushTokenAsync()).data;
-
+      console.log("PUSH TOKEN:");
       console.log(token);
-
       setPushToken(token);
+
+      // Grab the IDFV and store it in state (In a real app check if iOS b4 using)
+      const idfv = await Application.getIosIdForVendorAsync();
+      console.log("IDFV:");
+      console.log(idfv);
+      setIdfv(idfv);
     } else {
       alert("Must use physical device for Push Notifications");
     }
 
-    // Android gives us bonus configuration options, so we can config them here. 
+    // Android gives us bonus configuration options, so we can config them here.
     if (Platform.OS === "android") {
       Notifications.setNotificationChannelAsync("default", {
         name: "default",
@@ -65,7 +72,7 @@ export default function App() {
     }
   };
 
-  // Upon rendering, register for push. 
+  // Upon rendering, register for push.
   useEffect(() => {
     registerForPushNotificationsAsync();
 
@@ -101,6 +108,9 @@ export default function App() {
     >
       <Text>Device Push Token:</Text>
       <Text>{pushToken}</Text>
+
+      <Text>IDFV:</Text>
+      <Text>{idfv}</Text>
     </View>
   );
 }
